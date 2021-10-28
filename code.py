d3aeec5d68d8203ce7a53e6ef7693138a6b66cbc
hunterz-killer <82221655+hunterz-killer@users.noreply.github.com>
2021-10-28 15:50:35 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,16 @@
 import itertools
+import time
 
-rqpwd = "abc123"
+start_time = time.time()
+rqpwd = "abc123"  # replace it 
 
 charts = "abcdefghijklmnopqrstuvxyz1234567890"
 digits = 6
 
 for pwd in itertools.product(charts, repeat=digits):
     c_pwd = "".join(pwd)
-    print(c_pwd)
-    if c_pwd != rqpwd:
-        print(c_pwd)
-    else:
+    if c_pwd == rqpwd:
+        end_time = time.time()
+        runtime = round(end_time - start_time, 2)
+        print("The Pwd is", c_pwd, "found in", runtime, "Seconds")
         break
-
-print("The Pwd is ", c_pwd)
